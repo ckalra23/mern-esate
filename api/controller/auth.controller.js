d82b51db2b985da4b702d26be1aca897024959bf
chinmay <chnmykalra@gmail.com>
2024-02-04 17:51:05 +0530

    updated sign in api

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,42 +1,45 @@
-const User=require('../models/user.model')
-const bcrpyt=require('bcrypt');
+const User = require('../models/user.model')
+const bcrpyt = require('bcrypt');
 const errorHandler = require('../utils/error');
-const jwt=require('jsonwebtoken')
-const dotenv=require('dotenv');
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv');
 dotenv.config();
-const signup=async(req,res,next)=>{
-    try{
-    const{username,email,password}=req.body;
-    const hashedPassword= bcrpyt.hashSync(password,10);
-    const newUser=await User.create({
-        username,
-        email,
-        password:hashedPassword
-    })
-    res.status(201).json("User created successfully");
-}
-    catch(err){
+const signup = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+        const hashedPassword = bcrpyt.hashSync(password, 10);
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        })
+        res.status(201).json("User created successfully");
+    }
+    catch (err) {
         next(err);
     }
 
 }
-const signin=async(req,res,next)=>{
-    try{
-    const{email,password}=req.body;
-    const validUser=await User.findOne({email})
-    if(!validUser){
-        return next(errorHandler(404,'User not found'))
+const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'))
+        }
+        const validPassword = bcrpyt.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Wrong credentials!'));
+        }
+        const token = jwt.sign({ id: validPassword._id }, process.env.JWT_SECRET);
+        const{password:pass,...rest}=validUser._doc
+        res.
+            cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60 * 1000) })
+            .status(200).
+            json(rest);
     }
-    const validPassword=bcrpyt.compareSync(password,validUser.password);
-    if(!validPassword){
-        return next(errorHandler(401,'Wrong credentials!'));
-    }
-    const token=jwt.sign({id:validPassword._id},process.env.JWT_SECRET);
-    res.cookie('access_token',token,{httpOnly:true,expires:new Date(Date.now()+24*60*60*1000)})
-    .status(200).json('Valid user');
-    }
-    catch(err){
-        next(err); 
+    catch (err) {
+        next(err);
     }
 }
-module.exports={signup,signin}
+module.exports = { signup, signin }
