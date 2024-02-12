7e16bd5d5e89ab1aa626b1ebe11557a759d707e3
chinmay <chnmykalra@gmail.com>
2024-02-12 14:17:07 +0530

    created search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
-const{createListing,deleteListing,updateListing, getListing}=require('../controller/listing.controller')
+const{createListing,deleteListing,updateListing, getListing,getSearchListings}=require('../controller/listing.controller')
 const verifyToken=require('../utils/verifyToken')
 router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deleteListing)
 router.post('/update/:id',verifyToken,updateListing,getListing);
 router.get('/get/:id',getListing);
+router.get('/get',getSearchListings)
 module.exports=router;
