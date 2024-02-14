a48c7cd5631efd82dcb6bcf7b1f717e97f20c580
chinmay <chnmykalra@gmail.com>
2024-02-14 15:51:09 +0530

    added delete listing functionality if user is deleted

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -37,8 +37,9 @@ const deleteUser=async(req,res,next)=>{
     }
     try{
         await User.findByIdAndDelete(req.params.id);
+        await Listing.deleteMany({userRef:req.params.id})
         res.clearCookie('access_token');
-        res.status(200).json('User has been deleted')
+        res.status(200).json('User and associated listings has been deleted successfully')
     }
     catch(error){
         next(error);
