05fa0cc93ffe593d75b201f31d0d6ede73168330
chinmay <chnmykalra@gmail.com>
2024-02-16 20:47:09 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const cookieParser=require('cookie-parser');
 const userRouter=require('./routes/user.route')
 const authRouter=require('./routes/auth.route')
 const listRouter=require('./routes/listing.route')
-
+const path=require('path');
 dotenv.config();
 const app=express();
 mongoose.connect(process.env.MONGO)
@@ -16,11 +16,21 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 })
 
+const __dirname=path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
+app.listen(3000,()=>{
+    console.log("service running at port 3000!!");
+})
+
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listRouter)
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 //middleware
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
@@ -32,6 +42,3 @@ app.use((err,req,res,next)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log("service running at port 3000!!");
-})
