1762a49875036b9f207299c383dd23fa64ff5a1f
chinmay <chnmykalra@gmail.com>
2024-02-19 22:42:35 +0530

    unsuccessful deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-      target:'https://mern-estate-rllo.onrender.com',
+      target:'http://localhost:3000',
       secure:false
     },
   },
