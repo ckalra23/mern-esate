d57c9b8034d1e056bbbc01baa440b4e7f9f77aed
chinmay <chnmykalra@gmail.com>
2024-02-28 20:34:08 +0530

    deployment bug fixes

--- api/index.js
+++ api/index.js
@@ -6,7 +6,17 @@ const userRouter=require('./routes/user.route')
 const authRouter=require('./routes/auth.route')
 const listRouter=require('./routes/listing.route')
 const path=require('path');
+const winston = require('winston');ß
 dotenv.config();
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.Console(),
+      // You can add additional transports here, such as file or database transports
+    ],
+  });
 
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -26,10 +36,17 @@ app.listen(3000,()=>{
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listRouter)
-app.use(express.static(path.join(__dirname, 'client','dist')));
+app.use(express.static(path.join(__dirname,'..','client','dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-  })
+    const indexPath = path.join(__dirname, '..', 'client', 'dist', 'index.html');
+    logger.info(`Serving index.html from path: ${indexPath}`);
+    res.sendFile(indexPath, (err) => {
+      if (err) {
+        logger.error(`Error serving index.html: ${err.message}`);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+  });
 //middleware
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
